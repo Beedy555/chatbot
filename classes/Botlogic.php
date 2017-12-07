906e58ced6080c64e3d3e38017de59f93a27c904
Matthew Lambert <matthewlambert@redweb.com>
2017-12-07 14:07:14 +0000

    further updates

--- classes/Botlogic.php
+++ classes/Botlogic.php
@@ -3,48 +3,46 @@
 /**
 * 
 */
-class Botlogic
-{	
+class Botlogic {
 
-	var $response_template = array(
-		"speech" => "",
-		"displayText" => "",
-		"data" => array(),
-		"contextOut" => "",
-		"source" => "",
-	);
+  var $response_template = array(
+    "speech" => "",
+    "displayText" => "",
+    "data" => array(),
+    "contextOut" => "",
+    "source" => "",
+  );
 
-	function __construct() {
+  function __construct() {
 
-	}
 
-	function find_intent($data)
-	{
-		return $data['result']['action'];
-	}
+    function find_intent($data) {
+      return $data['result']['action'];
+    }
 
-	function format_reponse($speech = '', $display_text = '', $data = array(), $context_out = array(), $source = '') {
-		$this->response_template = array(
-			"speech" => $speech,
-			"displayText" => $display_text,
-			"data" => $data,
-			"contextOut" => $context_out,
-			"source" => $source
-		);
+    function format_reponse($speech = '', $display_text = '', $data = array(), $context_out = array(), $source = '') {
+      $this->response_template = array(
+        "speech" => $speech,
+        "displayText" => $display_text,
+        "data" => $data,
+        "contextOut" => $context_out,
+        "source" => $source
+      );
 
-		return $this->response_template;
-	}
+      return $this->response_template;
+    }
 
-	function get_all_dogs()
-	{
-		return $this->format_reponse(
-		);
-	}
+    function get_all_dogs() {
+      $all_dog_data = file_get_contents('https://www.battersea.org.uk/kioskpets/dogs');
+      $all_dogs = json_decode($all_dog_data);
+      return $this->format_reponse();
+    }
 
-	function get_all_cats()
-	{
-		return $this->format_reponse(
-		);
-	}
+    function get_all_cats() {
+      $all_cat_data = file_get_contents('https://www.battersea.org.uk/kioskpets/cats');
+      $all_cats = json_decode($all_cat_data);
+      return $this->format_reponse();
+    }
 
+  }
 }
